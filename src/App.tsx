6be749a9bf1fb9d9b3b78204850f0ec4903e301b
19920143367 <https://gitee.com>
2020-08-16 15:33:25 +0800

    tijiaoyige

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,29 @@ import air from './assets/airpods.png'
 import ip from './assets/iphone.png'
 import tab from './assets/tablet.png'
 
-const imgUrl=[air,ip,tab]
+const imgUrl=[
+  {
+    img:air,
+    title:'air标题',
+    describe:'air描述',
+    color:'#000',
+    bgColor:'#f1f1f3'
+  },
+  {
+    img:ip,
+    title:'ip标题',
+    describe:'ip描述',
+    color:'#fff',
+    bgColor:'#111111'
+  },
+  {
+    img:tab,
+    title:'tab标题',
+    // describe:'tab描述',
+    color:'#000',
+    bgColor:'#fafafa'
+  }
+]
 function App() {
   return <div className="App">
     <Carousel imgUrl={imgUrl}/>
